8cba3be4bcbea383960d8804b3d0c97f39982f44
Axel García <axelgar840@gmail.com>
2026-08-29 21:54:43 -0600

    feat(users): add UsersService with create and findByEmail methods

---
+++ src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsEnum, MinLength } from 'class-validator';
+import { UserRole } from 'src/modules/entities/user.entity';
+
+export class CreateUserDto {
+    @IsEmail()
+    email!: string;
+
+    @MinLength(8)
+    password!: string;
+
+    @IsEnum(UserRole)
+    role!: UserRole;
+}
---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { User } from '../entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ) {}
+
+    async create(dto: CreateUserDto): Promise<User> {
+        const passwordHash = await bcrypt.hash(dto.password, 10);
+
+        const user = this.userRepository.create({
+            email: dto.email,
+            passwordHash,
+            role: dto.role,
+        });
+
+        return this.userRepository.save(user);
+    }
+
+    async findByEmail(email: string): Promise<User | null> {
+        return this.userRepository.findOne({ where: { email } });
+    }
+}
